e0f3c49a618233948d592a24d0517b03966ceb27
xiaofei <lixiaofei0627@gmail.com>
2022-04-29 17:46:17 +0800

    add html

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -12,10 +12,23 @@ class Index {
     ctx.body = file
   }
   /**
-   * 列表
+   * 下载
    * @param ctx
    */
   async download(ctx: CTX) {
+    const filePath = path.join(__dirname, 'user.ts')
+    const file = fs.createReadStream(filePath)
+    const mimeType = mime.lookup(filePath) as string
+    ctx.response.set('content-type', mimeType)
+    ctx.response.set('code', '200')
+    ctx.response.set('msg', 'success')
+    ctx.body = file
+  }
+  /**
+   * 上传
+   * @param ctx
+   */
+  async upload(ctx: CTX) {
     const filePath = path.join(__dirname, 'user.ts')
     const file = fs.createReadStream(filePath)
     const mimeType = mime.lookup(filePath) as string
--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -3,6 +3,7 @@ import Index from '../../controller/index'
 const index = (router: Router) => {
   router.get('/', Index.home)
   router.get('/download', Index.download)
+  router.post('/upload', Index.upload)
 }
 
 export default index
